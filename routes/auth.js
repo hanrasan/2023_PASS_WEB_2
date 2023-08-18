d7c3ff27cbf5dee47858bb2dddf46bbc258fe8f8
leeyoubin112 <jeabin1121@gmail.com>
2023-08-18 15:40:44 +0900

    modified on a branch

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 module.exports = {
     CheckAuth: async function(req, res, next) {
-        console.log('auth');
+        console.log(req);
         if(req.isAuthenticated()) {
+            console.log('로그인 된 사용자입니다.');
             next();
         }
         else {
